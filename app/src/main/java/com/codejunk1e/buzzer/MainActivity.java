fe614e52d1745ad7c1066a5c91dbbe4b36582767
Robin <emeka14real@gmail.com>
2019-10-01 22:23:06 +0100

    Initial commit

---
+++ app/src/main/java/com/codejunk1e/buzzer/MainActivity.java
@@ -0,0 +1,31 @@
+package com.codejunk1e.buzzer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+
+            // Using handler with postDelayed called runnable run method
+
+            @Override
+
+            public void run() {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();        // close this activity
+            }
+
+        },  2000); // wait for 3 seconds
+    }
+}
